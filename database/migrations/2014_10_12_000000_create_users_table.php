d0085cb7f5fb7aefcdf3f5a5adbdf26e6c719cfc
afaizulh <ijulhakim14@gmail.com>
2024-04-14 10:42:06 +0300

    menambahkan migrasi

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,20 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->unsignedBigInteger('id_address');
+            $table->string('first_name');
+            $table->string('last_name')->nullable();
+            $table->string('role')->enum(['admin', 'user']);
             $table->string('email')->unique();
+            $table->int('phone_number', 15);
+            $table->string('gender')->enum(['pria', 'wanita']);
+            $table->int('birth_date');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('id_address')->references('id')->on('address');
         });
     }
 
---
+++ database/migrations/2024_04_14_062703_create_categories_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('list_size')->enum(['mini', 'big', '1/4kg', '1/2kg', '1kg']);
+            $table->string('list_flavour')->enum(['chocolate', 'strawberry', 'vanilla', 'barbeque', 'pedas', 'original']);
+            $table->string('list_menu')->enum(['pisang', 'makaroni', 'basreng', 'ususkrispi']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
